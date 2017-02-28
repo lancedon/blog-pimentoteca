6e865494d60fc30150f18b383c014c9cce4e4370
¨Leandro¨ <lancedon@gmail.com>
2017-02-28 09:55:05 -0300

    change face adds

--- dados_jogos/js/face.js
+++ dados_jogos/js/face.js
@@ -44,7 +44,7 @@ const FBaccessToken = '57532cae9b1cb2f2938da5791df1c238';
 						        window.location.href = destino;
 						    }
 						    
-						}, {scope: 'user_photos, publish_actions'}); 
+						}, {scope: 'user_photos'});  //publish_actions
 
 			      	}
 			};

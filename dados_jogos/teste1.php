72840b9d6f2882eac292895a11ba35a1294cea01
¨Leandro¨ <lancedon@gmail.com>
2017-02-28 10:14:32 -0300

    added jogo teste1

--- dados_jogos/teste1.php
+++ dados_jogos/teste1.php
@@ -1,7 +1,7 @@
 <?php
 //informacoes de debug, nao descomentar
-//error_reporting(E_ALL & ~E_NOTICE);
-//ini_set('display_errors', '1');
+error_reporting(E_ALL & ~E_NOTICE);
+ini_set('display_errors', '1');
 
 
 #defina aqui o local onde estao todas as imagens do teste

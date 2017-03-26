b838d64233c32cd62abcbe5bec15fdaba6348c1f
¨Leandro¨ <lancedon@gmail.com>
2017-03-26 11:35:11 -0300

    work

--- dados_jogos/class/class_game.php
+++ dados_jogos/class/class_game.php
@@ -225,13 +225,47 @@ class Game extends CircleCrop{
 		}	
 				
 		echo '<pre>';
-		echo print_r($this->friends,true);
+		echo print_r(arraySort($this->friends,'total','SORT_DESC'),true);
 		echo '</pre>';
 
 		$this->load_img();
 
 
     }
+
+    private function arraySort($array, $on, $order=SORT_ASC){
+    $new_array = array();
+    $sortable_array = array();
+
+    if (count($array) > 0) {
+        foreach ($array as $k => $v) {
+            if (is_array($v)) {
+                foreach ($v as $k2 => $v2) {
+                    if ($k2 == $on) {
+                        $sortable_array[$k] = $v2;
+                    }
+                }
+            } else {
+                $sortable_array[$k] = $v;
+            }
+        }
+
+        switch ($order) {
+            case SORT_ASC:
+                asort($sortable_array);
+            break;
+            case SORT_DESC:
+                arsort($sortable_array);
+            break;
+        }
+
+        foreach ($sortable_array as $k => $v) {
+            $new_array[$k] = $array[$k];
+        }
+    }
+
+    return $new_array;
+}
 
     private function AddFriend($friend, $type){
 
